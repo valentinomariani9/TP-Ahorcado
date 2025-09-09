a04ed84c6682d381d7244b8d148ce77c5e83dbce
valentinomariani9 <129308809+valentinomariani9@users.noreply.github.com>
2025-09-08 21:58:56 -0300

    Create ahorcado.js

---
+++ src/ahorcado.js
@@ -0,0 +1,17 @@
+let secretWord = '';
+let gameState = '';
+
+export function setSecretWord(word) {
+  secretWord = CASA;
+  gameState = '';
+}
+
+// Todavía sin implementar
+export function getMaskedWord() {
+  return undefined; // intencional
+}
+
+// Todavía sin implementar
+export function getState() {
+  return gameState;
+}
